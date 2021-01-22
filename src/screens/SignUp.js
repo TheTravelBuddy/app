eaa5d8ca766f1b98f372a4c0b38df49f661ff430
Divy Jain <dkj@somaiya.edu>
2021-01-23 01:27:17 +0530

    ✨ add register functionality

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { View } from "react-native";
 import { TextInput } from "react-native-paper";
 
 import styles from "./styles/authStyles";
 import { Scaffold, Button, Title, Label } from "../components";
+import useTextInput from "../hooks/useTextInput";
+import useToggle from "../hooks/useToggle";
 import { useAuth } from "../stores/Auth";
 
 const SignUpScreen = () => {
   const logout = useAuth((state) => state.logout);
+  const register = useAuth((state) => state.register);
+  const logoutLoading = useToggle(false);
+  const registerLoading = useToggle(false);
+  const name = useTextInput();
+  const dob = useTextInput();
+  const gender = useTextInput();
+  const mood = useTextInput();
+
+  const handleLogout = useCallback(() => {
+    logoutLoading.start();
+    logout().catch(() => {
+      logoutLoading.stop();
+    });
+  }, [logoutLoading, logout]);
+
+  const handleRegister = useCallback(() => {
+    registerLoading.start();
+    register({
+      name: name.value,
+      dob: dob.value,
+      gender: gender.value,
+    }).catch(() => {
+      registerLoading.stop();
+    });
+  }, [name.value, dob.value, gender.value, registerLoading, register]);
 
   return (
     <Scaffold>
@@ -17,30 +44,48 @@ const SignUpScreen = () => {
         </View>
         <View>
           <Label>Before we start, tell us something about you.</Label>
-          <TextInput label="Name" style={styles.FormInput} />
+          <TextInput
+            label="Name"
+            {...name.props}
+            disabled={logoutLoading.state || registerLoading.state}
+            style={styles.FormInput}
+          />
           <View style={styles.FormInputContainer}>
             <TextInput
+              {...dob.props}
+              disabled={logoutLoading.state || registerLoading.state}
               label="DOB"
               style={[styles.FormInput, styles.FormInputLeft]}
             />
             <TextInput
+              {...gender.props}
+              disabled={logoutLoading.state || registerLoading.state}
               label="Gender"
               style={[styles.FormInput, styles.FormInputRight]}
             />
           </View>
-          <TextInput label="Prefered Seasons" style={styles.FormInput} />
+          <TextInput
+            label="Mood"
+            {...mood.props}
+            disabled={logoutLoading.state || registerLoading.state}
+            style={styles.FormInput}
+          />
           <View style={styles.FormInputContainer}>
             <Button
+              disabled={logoutLoading.state || registerLoading.state}
+              loading={logoutLoading.state}
               style={styles.FormInputLeft}
-              onPress={logout}
+              onPress={handleLogout}
               mode="outlined"
             >
               Cancel
             </Button>
             <Button
+              disabled={logoutLoading.state || registerLoading.state}
+              loading={registerLoading.state}
               style={styles.FormInputRight}
               mode="contained"
-              onPress={() => {}}
+              onPress={handleRegister}
             >
               Save
             </Button>
