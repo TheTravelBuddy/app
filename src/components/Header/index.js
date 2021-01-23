25982bd65c47dfa3f2f448f21568ab2af083fba3
riddhii15 <riddhi.dholakia@somaiya.edu>
2021-01-23 20:57:13 +0530

    Header updated

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header as Appbar } from "react-native-paper";
+
+const Header = () => {
+  return (
+    <Appbar.Header>
+      <Appbar.Content title="Travel buddy" />
+    </Appbar.Header>
+  );
+};
+
+export default Header;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View } from "react-native";
+import { Appbar, useTheme } from "react-native-paper";
 
 import styles from "./styles/authStyles";
 import { Scaffold, Button, Title } from "../components";
@@ -7,12 +8,20 @@ import { useAuth } from "../stores/Auth";
 
 const HomeScreen = () => {
   const logout = useAuth((state) => state.logout);
-
+  const theme = useTheme();
   return (
-    <Scaffold>
+    <Scaffold
+      renderHeader={() => (
+        <Appbar.Header
+          style={{ backgroundColor: theme.colors.background, elevation: 0 }}
+        >
+          <Appbar.Content title="Travel Buddy" />
+        </Appbar.Header>
+      )}
+    >
       <View style={styles.Container}>
         <View style={styles.Header}>
-          <Title style={styles.HeaderTitle}>Home!</Title>
+          <Title style={styles.HeaderTitle}>Travel Buddy</Title>
         </View>
         <Button mode="contained" onPress={logout}>
           SIGN OUT
