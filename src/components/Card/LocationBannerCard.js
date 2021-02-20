fb5e9422f5274636a94d2a663588481ac9c57d57
tinchanalkar <t.inchanalkar@somaiya.edu>
2021-02-20 15:51:28 +0530

    packagedetails-screen-ui

--- src/components/Card/LocationBannerCard.js
+++ src/components/Card/LocationBannerCard.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { View } from "react-native";
 import { Card } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 
 import styles from "./styles";
 import RatingPill from "../RatingPill";
@@ -8,10 +9,19 @@ import CardTitle from "../Typography/CardTitle";
 import useScreenDimensions from "../../hooks/useScreenDimensions";
 import { SCREEN_PADDING } from "../../constants";
 
-const LocationBannerCard = ({ name, coverUri, rating, style }) => {
+const LocationBannerCard = ({ id, name, coverUri, rating, style }) => {
   const { width } = useScreenDimensions();
+  const { navigate } = useNavigation();
+
+  const goToPackage = useCallback(() => {
+    navigate("PackageDetailsScreen", { packageId: id });
+  }, [id, navigate]);
+
   return (
-    <Card style={[{ width: width - 2 * SCREEN_PADDING }, style]}>
+    <Card
+      style={[{ width: width - 2 * SCREEN_PADDING }, style]}
+      onPress={goToPackage}
+    >
       <Card.Cover
         style={{ height: Math.round(width / 2 - SCREEN_PADDING) }}
         source={{ uri: coverUri }}
