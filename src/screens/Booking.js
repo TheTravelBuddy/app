a768479fc1cb6c45f3484797543715ef1274bd98
Divy Jain <dkj@somaiya.edu>
2021-03-30 00:05:18 +0530

    Connect BookingScreen to API

--- src/screens/Booking.js
+++ src/screens/Booking.js
@@ -10,81 +10,13 @@ import {
   PackageHalfCard,
   HotelDetailCard,
   HorizontalScroller,
+  RenderOnLoad,
 } from "../components";
-
-const packagesData = [
-  {
-    id: 1,
-    coverUri:
-      "https://static.toiimg.com/photo/77652252/oie_2095710glh9nB1O.jpg?width=748&resize=4",
-    name: "Beauty of South",
-    rating: 3.5,
-  },
-  {
-    id: 2,
-    coverUri:
-      "https://images.news18.com/ibnlive/uploads/2016/07/Chitkul-Valley-Himachal-PradeshIndia-Edited-in-Lightroom-5-Imgur.jpg",
-    name: "Magic of the North",
-    rating: 3.5,
-  },
-  {
-    id: 3,
-    coverUri: "https://picsum.photos/1005",
-    name: "Magic of the North",
-    rating: 3.5,
-  },
-];
-
-const topRatedPackagesData = [
-  {
-    id: 1,
-    coverUri:
-      "https://static.toiimg.com/photo/msid-52005539,width-96,height-65.cms",
-    name: "Beauty of South",
-    rating: 4.5,
-  },
-  {
-    id: 2,
-    coverUri: "https://static.toiimg.com/photo/24476893.cms",
-    name: "Magic of North",
-    rating: 4.9,
-  },
-];
-
-const hotelDetailsData = [
-  {
-    id: 1,
-    coverUri:
-      "https://media-cdn.tripadvisor.com/media/photo-m/1280/1b/a5/d8/c1/exterior.jpg",
-    name: "Taj Mahal Palace",
-    rating: 4.5,
-    locality: "Colaba",
-    city: "Mumbai",
-    price: 3550,
-  },
-  {
-    id: 2,
-    coverUri:
-      "https://imgcy.trivago.com/c_lfill,d_dummy.jpeg,e_sharpen:60,f_auto,h_450,q_auto,w_450/itemimages/27/36/2736904_v5.jpeg",
-    name: "JW Marriott",
-    rating: 5,
-    locality: "Colaba",
-    city: "Mumbai",
-    price: 4000,
-  },
-  {
-    id: 3,
-    coverUri:
-      "https://imgcy.trivago.com/c_lfill,d_dummy.jpeg,e_sharpen:60,f_auto,h_450,q_auto,w_450/itemimages/99/50/99501_v5.jpeg",
-    name: "Adarsh Baug Hotel",
-    rating: 3.5,
-    locality: "Kalbadevi",
-    city: "Mumbai",
-    price: 1050,
-  },
-];
+import { useAPI } from "../helpers/API";
 
 const BookingScreen = ({ navigation: { navigate } }) => {
+  const [apiRequest] = useAPI("/traveller/booking");
+
   return (
     <Scaffold
       header={useMemo(
@@ -100,56 +32,60 @@ const BookingScreen = ({ navigation: { navigate } }) => {
         [navigate]
       )}
     >
-      <View style={styles.Section}>
-        <SectionHeader style={[styles.ScreenPadded, styles.SectionHeader]}>
-          Recommended Packages
-        </SectionHeader>
-        <HorizontalScroller>
-          {packagesData.map(({ id, coverUri, name, rating }) => (
-            <PackageBannerCard key={id} {...{ id, coverUri, name, rating }} />
-          ))}
-        </HorizontalScroller>
-      </View>
-      <View style={styles.Section}>
-        <SectionHeader style={[styles.ScreenPadded, styles.SectionHeader]}>
-          Hotels Nearby
-        </SectionHeader>
-        <HorizontalScroller>
-          {hotelDetailsData.map(
-            ({ id, coverUri, name, rating, locality, city, price }) => (
-              <HotelDetailCard
-                key={id}
-                {...{ id, coverUri, name, rating, locality, city, price }}
-              />
-            )
-          )}
-        </HorizontalScroller>
-      </View>
-      <View style={styles.Section}>
-        <SectionHeader style={[styles.ScreenPadded, styles.SectionHeader]}>
-          Highest Rated Packages
-        </SectionHeader>
-        <HorizontalScroller>
-          {topRatedPackagesData.map(({ id, coverUri, name, rating }) => (
-            <PackageHalfCard key={id} {...{ id, coverUri, name, rating }} />
-          ))}
-        </HorizontalScroller>
-      </View>
-      <View style={styles.Section}>
-        <SectionHeader style={[styles.ScreenPadded, styles.SectionHeader]}>
-          Hotels on a Budget
-        </SectionHeader>
-        <HorizontalScroller>
-          {hotelDetailsData.map(
-            ({ id, coverUri, name, rating, locality, city, price }) => (
-              <HotelDetailCard
-                key={id}
-                {...{ id, coverUri, name, rating, locality, city, price }}
-              />
-            )
-          )}
-        </HorizontalScroller>
-      </View>
+      <RenderOnLoad loading={!apiRequest.data}>
+        {() => (
+          <>
+            <View style={styles.Section}>
+              <SectionHeader
+                style={[styles.ScreenPadded, styles.SectionHeader]}
+              >
+                Recommended Packages
+              </SectionHeader>
+              <HorizontalScroller>
+                {apiRequest.data?.recommendedPackages.map((packageData) => (
+                  <PackageBannerCard key={packageData.id} {...packageData} />
+                ))}
+              </HorizontalScroller>
+            </View>
+            <View style={styles.Section}>
+              <SectionHeader
+                style={[styles.ScreenPadded, styles.SectionHeader]}
+              >
+                Hotels Nearby
+              </SectionHeader>
+              <HorizontalScroller>
+                {apiRequest.data?.nearbyHotels.map((hotelData) => (
+                  <HotelDetailCard key={hotelData.id} {...hotelData} />
+                ))}
+              </HorizontalScroller>
+            </View>
+            <View style={styles.Section}>
+              <SectionHeader
+                style={[styles.ScreenPadded, styles.SectionHeader]}
+              >
+                Highest Rated Packages
+              </SectionHeader>
+              <HorizontalScroller>
+                {apiRequest.data?.topPackages.map((packageData) => (
+                  <PackageHalfCard key={packageData.id} {...packageData} />
+                ))}
+              </HorizontalScroller>
+            </View>
+            <View style={styles.Section}>
+              <SectionHeader
+                style={[styles.ScreenPadded, styles.SectionHeader]}
+              >
+                Hotels on a Budget
+              </SectionHeader>
+              <HorizontalScroller>
+                {apiRequest.data?.budgetHotels.map((hotelData) => (
+                  <HotelDetailCard key={hotelData.id} {...hotelData} />
+                ))}
+              </HorizontalScroller>
+            </View>
+          </>
+        )}
+      </RenderOnLoad>
     </Scaffold>
   );
 };
